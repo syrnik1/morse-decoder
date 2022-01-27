779f89cc667672b0c5f3178adfd7f6b88cbdf3db
syrnik1 <95712566+syrnik1@users.noreply.github.com>
2022-01-27 17:43:34 +0300

    update2

--- src/index.js
+++ src/index.js
@@ -41,16 +41,16 @@ const decodeLetter = letter => {
     let letter_code = letter.match(/.{1,2}/g).map(v => {
       if (v === '10') return '.';
       if (v === '11') return '-';
-    }).join( '' );
+    }).join('');
     return MORSE_TABLE[letter_code];
   }
   
   const decodeWord = word => {
-    return word.match(/.{1,10}/g).map(decodeLetter).join( '' );
+    return word.match(/.{1,10}/g).map(decodeLetter).join('');
   }
   
   function decode(expr) {
-    return expr.split('**********').map(decodeWord).join( ' ' );
+    return expr.split('**********').map(decodeWord).join(' ');
   }
   
 
